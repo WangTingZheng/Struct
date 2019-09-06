3ad99a817ce5f9ebd0edaa4b37ccc91be3e2ddce
Engou <2251985371@qq.com>
2019-09-06 12:59:47 +0800

    v1.1.2 更新了： 1、添加.vs的忽略

--- struct.h
+++ struct.h
@@ -1,7 +1,10 @@
 #ifndef __STRUCT_H_
 #define __STRUCT_H_
 
-
+NextPageNode newNextPage(bool CON, int ID, NextPageNode* NEXT);
+NextPageNode addNode(NextPageNode* HEAD, NextPageNode* next);
+NextPageNode detectNode(NextPageNode* HEAD, int id);
+Page newPage(char NAME[], int ID, NextPageNode* HEAD);
 
 #endif
 
